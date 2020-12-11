a84229d75b42ac0f6166cafc08a3ca1487b9cfa6
Bicanic-Alen <bicanic.alen@iisgalvanimi.edu.it>
2020-12-11 13:14:10 +0000

    3.5

--- first-routed-app/src/environments/environment.ts
+++ first-routed-app/src/environments/environment.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
   production: false,
-  oauthToken: "Bearer BQCTcra-CwJ0zSaWMyGVObVzVk2_-tgRjvUWNvLpJI4hhy8Dnr0fL0H0Z1oA0owrNenY5yjkY3f3260ie3RTytt3uKVQO8e0urmE6qOGaBSmtwwbOU94PiapYfukpW5Zt0hu175FZYdc8rheZ68ekEO7OsdLUapO9xE"
+  oauthToken: "Bearer BQDzQycKbMwN_jDe20dqr0dk7I-8e8WlM3DdvItW0zH04hlO6Rg-tPwQC8FxrNXN0cDQoIrm8SMA2pheBeGKM9hJyRz2J0cBZxoJY-3WSUMPZlV4jJJKbxg_mLzX4VpGFV8DJKN4TOdunsZSW7KFX42A44ZFD0fHkr4"
 };
